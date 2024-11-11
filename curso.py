08349e8e0ade1ada743195e16703d86fcf99f973
juantoledo1 <juanmanueltoledo0487@gmail.com>
2024-11-11 19:43:56 -0300

    primer paso en python con conda

---
+++ curso.py
@@ -0,0 +1,20 @@
+# print("hola mundo como estan estoy practicando python Y ESOTY MUY FELIZ")
+# x = 10
+# c = 55
+# #vamos a robar la suma ahora
+
+# print(x + c)
+
+# nombre = "juan"
+# apellido =  "toledo"
+    
+# print(f"hola mi nombre de usuario es {nombre} y mi apellido es {apellido}")
+
+# x = 3 + 1j
+# print (x)
+
+nombre = input("cual es tu nombre completo: ")
+edad= int(input("cual es tu edad: "))
+altura= float(input("cual es tu altura exacta: "))
+
+print(f"hola mi nombre es: {nombre} mi edad es: {edad} mi altura exacta es: {altura}")
